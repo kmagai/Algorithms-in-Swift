3677c5d6c6870efd346b8e9f20aab0cf531cf33f
koki <k.k.cutt@gmail.com>
2014-09-19 18:55:39 +0900

    Add selection sort

---
+++ selectionSort.playground/section-1.swift
@@ -0,0 +1,21 @@
+var Numbers = [23, 53, 11, 62, 32, 86, 13, 7, 80, 75, 67, 27, 85, 31, 22, 5, 35, 51, 29, 59]
+
+
+func selectionSort(var nums:[Int]) -> [Int] {
+    for i in (0...nums.count-1) {
+        var x = nums[i]
+        var j = i, s = nums[i], si = i
+        while j < nums.count - 1 {
+            if nums[j] < s {
+                s = nums[j]
+                si = j
+            }
+            j++
+        }
+        nums[i] = s
+        nums[si] = x
+    }
+    return nums
+}
+
+selectionSort(Numbers)
